420387f10e5dcc125158108e4932f535616ecdda
gaohongquan <gao.hongquan@qq.com>
2020-05-23 16:52:23 +0800

    更改工程到 2019

--- Assets/StencilDecals/Runtime/DecalRenderer.cs
+++ Assets/StencilDecals/Runtime/DecalRenderer.cs
@@ -5,51 +5,74 @@ using UnityEngine.Rendering;
 
 namespace Yunchang
 {
+    [ExecuteInEditMode]
     [DisallowMultipleComponent]
     [RequireComponent(typeof(Camera))]
-    public class RendererFeatures : MonoBehaviour
+    public class DecalRenderer : MonoBehaviour
     {
+        /// <summary>
+        /// 是否支持法线
+        /// </summary>
+        public bool supportNormal = false;
+
         Camera m_Camera;
+
+        Material m_BlitCopyDepthMat;
 
         RenderTexture m_ColorTex;
         RenderTexture m_DepthTex;
         RenderTexture m_DepthCopyTex;
 
-        Material m_BlitCopyDepthMat;
-
         CommandBuffer m_CopyDepthPass;
         CommandBuffer m_FinelPass;
-        
+
+        void OnValidate()
+        {
+            OnDisable();
+            OnEnable();
+        }
+
         void OnEnable()
         {
             m_Camera = this.GetComponent<Camera>();
+
+            if (supportNormal)
+            {
+                Shader.EnableKeyword("_SUPPORT_NORMAL");
+                m_Camera.depthTextureMode = DepthTextureMode.DepthNormals;
+            }
+            else
+            {
+                Shader.DisableKeyword("_SUPPORT_NORMAL");
+                m_Camera.depthTextureMode = DepthTextureMode.Depth;
+            }
+
             //m_Camera.depthTextureMode = DepthTextureMode.Depth;
 
-            m_ColorTex = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32);
+            /*m_ColorTex = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGB32);
             m_ColorTex.Create();
             m_DepthTex = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.Depth);
             m_DepthTex.Create();
             m_Camera.SetTargetBuffers(m_ColorTex.colorBuffer, m_DepthTex.depthBuffer);
 
-            Shader.SetGlobalTexture("_CameraDepthTexture", m_DepthTex);
+            //Shader.SetGlobalTexture("_CameraDepthTexture", m_DepthTex);
 
             m_FinelPass = new CommandBuffer { name = "Finel Pass" };
             m_FinelPass.Blit(m_ColorTex, BuiltinRenderTextureType.None);
             m_Camera.AddCommandBuffer(CameraEvent.AfterImageEffects, m_FinelPass);
 
-            
             m_DepthCopyTex = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.RFloat);
             m_DepthCopyTex.Create();
             m_BlitCopyDepthMat = new Material(Shader.Find("Hidden/BlitCopyDepth"));
             m_CopyDepthPass = new CommandBuffer { name = "Copy Depth Pass"};
             m_CopyDepthPass.Blit(m_DepthTex, m_DepthCopyTex, m_BlitCopyDepthMat);
             m_CopyDepthPass.SetGlobalTexture("_CameraDepthTexture", m_DepthCopyTex);
-            m_Camera.AddCommandBuffer(CameraEvent.AfterForwardOpaque, m_CopyDepthPass);
+            m_Camera.AddCommandBuffer(CameraEvent.AfterForwardOpaque, m_CopyDepthPass);*/
         }
 
         void OnDisable()
         {
-            m_Camera.targetTexture = null;
+           /* m_Camera.targetTexture = null;
             m_Camera.RemoveCommandBuffer(CameraEvent.AfterImageEffects, m_FinelPass);
             m_ColorTex.Release();
             m_DepthTex.Release();
@@ -61,22 +84,12 @@ namespace Yunchang
                 m_DepthCopyTex.Release();
                 m_CopyDepthPass.Dispose();
                 Object.DestroyImmediate(m_BlitCopyDepthMat);
-            }
+            }*/
         }
 
         void Update()
         {
-            //decal.GetComponent<Renderer>().material.SetTexture("_DepthTex",m_DepthTex);
-        }
-
-        void OnPreRender()
-        {
-            //m_Camera.AddCommandBuffer(cameraEvent, m_FinelPass);
-        }
-
-        void OnPostRender()
-        {
-            //m_Camera.RemoveCommandBuffer(cameraEvent, m_FinelPass);
+            //Camera.main.depthTextureMode = DepthTextureMode.Depth;
         }
     }
 }
--- Assets/StencilDecals/Shaders/DecalShader.shader
+++ Assets/StencilDecals/Shaders/DecalShader.shader
@@ -20,6 +20,7 @@
 
 			CGPROGRAM
 			#pragma target 3.0
+			#pragma multi_compile _ _SUPPORT_NORMAL
 			#pragma vertex vert
 			#pragma fragment frag
 			
@@ -29,39 +30,70 @@
 			{
 				float4 pos : SV_POSITION;
 				half2 uv : TEXCOORD0;
-				float4 screenUV : TEXCOORD1;
+				float4 screenPos : TEXCOORD1;
 				float3 ray : TEXCOORD2;
 			};
 
 			v2f vert (float3 v : POSITION)
 			{
 				v2f o;
-				o.pos = UnityObjectToClipPos (float4(v,1));
+				o.pos = UnityObjectToClipPos (v);
 				o.uv = v.xz+0.5;
-				o.screenUV = ComputeScreenPos (o.pos);
-				o.ray = mul (UNITY_MATRIX_MV, float4(v,1)).xyz * float3(-1,-1,1);
+				o.screenPos = ComputeScreenPos (o.pos);
+				o.ray = mul(UNITY_MATRIX_MV, float4(v, 1)).xyz * float3(1,1,-1);
 				return o;
 			}
 
 			sampler2D _MainTex;
+
+#ifdef _SUPPORT_NORMAL
+			sampler2D _CameraDepthNormalsTexture;
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				i.ray = i.ray * (_ProjectionParams.z / i.ray.z);
+				float2 uv = i.screenPos.xy / i.screenPos.w;
+				float depth;
+				float3 viewNormal;
+
+				float4 encode = tex2D(_CameraDepthNormalsTexture, uv);
+				DecodeDepthNormal(encode, depth, viewNormal);
+
+				float4 vpos = float4(i.ray * depth,1);
+				float3 wpos = mul(unity_CameraToWorld, vpos).xyz;
+				float3 opos = mul(unity_WorldToObject, float4(wpos,1)).xyz;
+
+				clip(float3(0.5,0.5,0.5) - abs(opos.xyz));
+
+				float3 decalNormal = mul(UNITY_MATRIX_T_MV, viewNormal);
+				opos.xz -= opos.y * decalNormal.xz;
+				clip(float3(0.5, 0.5, 0.5) - abs(opos.xyz));
+
+				i.uv = opos.xz + 0.5;
+				fixed4 col = tex2D(_MainTex, i.uv);
+				return col;
+			}
+
+#else
 			sampler2D _CameraDepthTexture;
 
 			fixed4 frag(v2f i) : SV_Target
 			{
 				i.ray = i.ray * (_ProjectionParams.z / i.ray.z);
-				float2 uv = i.screenUV.xy / i.screenUV.w;
+				float2 uv = i.screenPos.xy / i.screenPos.w;
 				float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, uv);
-				depth = Linear01Depth (depth);
+				depth = Linear01Depth(depth);
 				float4 vpos = float4(i.ray * depth,1);
-				float3 wpos = mul (unity_CameraToWorld, vpos).xyz;
-				float3 opos = mul (unity_WorldToObject, float4(wpos,1)).xyz;
+				float3 wpos = mul(unity_CameraToWorld, vpos).xyz;
+				float3 opos = mul(unity_WorldToObject, float4(wpos,1)).xyz;
 
-				clip (float3(0.5,0.5,0.5) - abs(opos.xyz));
+				clip(float3(0.5,0.5,0.5) - abs(opos.xyz));
 
-				i.uv = opos.xz+0.5;
+				i.uv = opos.xz + 0.5;
 				fixed4 col = tex2D(_MainTex, i.uv);
 				return col;
 			}
+#endif
 			ENDCG
 		}		
 
